a359357b774f10789b7c2720c5335c9ad5900178
James Ware <jazz642@gmail.com>
2016-07-08 14:03:16 -0400

    SIS project

---
+++ MVC_SIS/Exercises/Controllers/StudentController.cs
@@ -0,0 +1,95 @@
+﻿using Exercises.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercises.Models.Data;
+using Exercises.Models.ViewModels;
+
+namespace Exercises.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            var model = StudentRepository.GetAll();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            var viewModel = new StudentVM();
+            viewModel.SetCourseItems(CourseRepository.GetAll());
+            viewModel.SetMajorItems(MajorRepository.GetAll());
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(StudentVM studentVM)
+        {
+            if (ModelState.IsValid)
+            {
+                studentVM.Student.Courses = new List<Course>();
+
+                foreach (var id in studentVM.SelectedCourseIds)
+                    studentVM.Student.Courses.Add(CourseRepository.Get(id));
+
+                studentVM.Student.Major = MajorRepository.Get(studentVM.Student.Major.MajorId);
+
+                StudentRepository.Add(studentVM.Student);
+
+                return RedirectToAction("List");
+            }
+
+            studentVM.SetCourseItems(CourseRepository.GetAll());
+            studentVM.SetMajorItems(MajorRepository.GetAll());
+            return View(studentVM);         
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var student = StudentRepository.Get(id);
+            var studentVM = new StudentVM();
+            studentVM.Student = student;
+            studentVM.SetCourseItems(CourseRepository.GetAll());
+            studentVM.SetMajorItems(MajorRepository.GetAll());
+
+            return View(studentVM);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(StudentVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var name = Request.Form["Student.FirstName"];
+                vm.Student.Courses = new List<Course>();
+
+                foreach (var id in vm.SelectedCourseIds)
+                    vm.Student.Courses.Add(CourseRepository.Get(id));
+
+                vm.Student.Major = MajorRepository.Get(vm.Student.Major.MajorId);
+                var studentVM = new StudentVM();       
+                StudentRepository.Edit(vm.Student);
+
+                return RedirectToAction("List");
+            }
+
+            vm.SetCourseItems(CourseRepository.GetAll());
+            vm.SetMajorItems(MajorRepository.GetAll());
+            return View(vm);
+        }
+
+    }
+}
---
+++ MVC_SIS/Exercises/Models/Data/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exercises.Models.Data
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal? GPA { get; set; }
+        public Address Address { get; set; }
+        public Major Major { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
